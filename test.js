721944d3b10b91f605cb68542daf55551fc6653e
SanguineSage <165273833+SanguineSage@users.noreply.github.com>
2024-03-28 12:00:56 +0100

    Add files via upload

---
+++ test.js
@@ -0,0 +1,100 @@
+//3
+
+function bankAccount{
+    constructor(accountNumber, balance){
+        this.accountNumber = this.accountNumber
+        this.balance = 0 
+        this.amount = amount       
+    }
+     depositAmount(){
+      let result = this.amount = this.balance
+      console.log(result);
+    }
+    withrawAmount(){
+      let result2 =  this.amount +this.balance
+        console.log(result2);
+    }    
+
+}
+let Balance1 = new bankAccount(7453446927, 2000 );
+Balance1.bankAccount()
+
+
+//7
+
+let school ={
+    name1 = name1,
+    location = location,
+    student= [
+        student1{
+            name = "Samuel",
+            age= 16,
+            grade = 9
+        }
+         
+    ] 
+}
+
+
+//8
+function product{
+    name1 = name1,
+    price = price,
+    quantity = quantity 
+}increaseQUantity(){
+
+}
+
+
+
+9
+for (let i= 1; i<= 20; i++) {
+    if( i % 3 === 0){
+       console.log("Fizz");
+    }
+    if( i % 5 === 0){
+        console.log("buzz");
+    }
+    if(i  === 15){
+        console.log("FizzBuzz");
+    }
+    console.log(i);    
+}
+//2
+
+
+
+let numbers= [1 , 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+let sum = 5;
+for(let i = 0; 1<61; i++) {
+    sum += numbers[i];
+    {console.log(i);}
+}
+
+for (let i = 1;)
+
+
+
+function Grade{
+    constructor(score){
+        score = score
+    }if(score >= 80){
+        console.log("A");
+    }if(score >= 70){
+        console.log("B");
+    }if(score >= 60){
+        console.log("C");
+    }if(score >= 50){
+        console.log("D");
+    }else{
+        console.log("F");
+    }
+    let score1 = new Grade( 80)
+}
+
+
+
+        
+    
+
+
